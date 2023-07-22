77f741ed4bd06fd4ba2e914388dfbd1b5e1a59db
Ali Zia <ziaali455@gmail.com>
2023-07-21 23:17:48 -0400

    first

---
+++ Madidi-Draft/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Madidi-Draft
+//
+//  Created by Ali Zia on 7/21/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func changeBackgroundColor(_ sender: UIButton) {
+        let randomColor = changeColor()
+        view.backgroundColor = randomColor
+        func changeColor() -> UIColor{
+
+                let red = CGFloat.random(in: 0...1)
+                let green = CGFloat.random(in: 0...1)
+                let blue = CGFloat.random(in: 0...1)
+
+                return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+            }
+    }
+    
+    
+}
+
